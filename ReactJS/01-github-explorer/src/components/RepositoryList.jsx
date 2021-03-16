24cec1b6fb5f4838b79a68c8d32da8a62ec8880d
luiz-ortega <luizngust@hotmail.com>
2021-03-15 21:22:51 -0300

    chore: add fast-refresh

---
+++ ReactJS/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,20 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: "unform",
+  description: "Forms in React",
+  link: "",
+};
+
+export function RepositoryList() {
+  return (
+    <section>
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+      </ul>
+    </section>
+  );
+}
