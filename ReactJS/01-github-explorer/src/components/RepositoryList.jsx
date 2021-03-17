930bc19e99e93e1303d3b817c795586aabd050cf
luiz-ortega <luizngust@hotmail.com>
2021-03-17 20:09:21 -0300

    feat: add repositories from github API

--- ReactJS/01-github-explorer/src/components/RepositoryList.jsx
+++ ReactJS/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,20 +1,24 @@
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
-
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "",
-};
+import { useEffect, useState } from "react";
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.id} repository={repository} />
+        ))}
       </ul>
     </section>
   );
