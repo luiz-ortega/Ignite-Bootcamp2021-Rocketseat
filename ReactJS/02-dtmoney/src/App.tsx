d76ca21b332164ea694a70a8f1bde40e3f2d0bdf
luiz-ortega <luizngust@hotmail.com>
2021-03-24 23:10:16 -0300

    style: add global styles

--- ReactJS/02-dtmoney/src/App.tsx
+++ ReactJS/02-dtmoney/src/App.tsx
@@ -1,3 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </div>
+  );
 }
