8e154b87883e8bc07dc923026cfc8bbe7677ecaa
luiz-ortega <luizngust@hotmail.com>
2021-04-02 17:52:40 -0300

    feat: add header component

--- ReactJS/02-dtmoney/src/App.tsx
+++ ReactJS/02-dtmoney/src/App.tsx
@@ -1,9 +1,10 @@
 import { GlobalStyle } from "./styles/global";
+import Header from "./components/Header";
 
 export function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header />
       <GlobalStyle />
     </div>
   );
