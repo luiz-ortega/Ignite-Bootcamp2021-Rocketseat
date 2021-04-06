1d2f8f7074f1b67d68b10c0d757b5d9a59936e01
luiz-ortega <luizngust@hotmail.com>
2021-04-05 21:50:47 -0300

    feat: add dynamic styles according with transaction type selected

--- ReactJS/02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ ReactJS/02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Container, TransactionTypeContainer } from "./styles";
+import React, { useState } from "react";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
@@ -14,6 +14,8 @@ export default function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [type, setType] = useState("deposit");
+
   return (
     <Modal
       isOpen={isOpen}
@@ -34,15 +36,23 @@ export default function NewTransactionModal({
         <input placeholder="Título" />
         <input type="number" placeholder="Valor" />
         <TransactionTypeContainer>
-          <button>
+          <RadioBox
+            type="button"
+            onClick={() => setType("deposit")}
+            isActive={type === "deposit"}
+          >
             <img src={incomeImg} alt="entrada" />
             <span>Entrada</span>
-          </button>
+          </RadioBox>
 
-          <button>
+          <RadioBox
+            type="button"
+            onClick={() => setType("withdraw")}
+            isActive={type === "withdraw"}
+          >
             <img src={outcomeImg} alt="saída" />
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
         <input placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
--- ReactJS/02-dtmoney/src/components/NewTransactionModal/styles.ts
+++ ReactJS/02-dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,6 +1,10 @@
 import { darken } from "polished";
 import styled from "styled-components";
 
+interface RadioBoxProps {
+  isActive: boolean;
+}
+
 export const Container = styled.form`
   h2 {
     color: var(--text-title);
@@ -54,29 +58,30 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
+`;
 
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
-    background: transparent;
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    transition: border-color 0.2s;
+  background: ${(props) => (props.isActive ? "#dedddd" : "transparent")};
 
-    &:hover {
-      border-color: ${darken(0.1, "#d7d7d7")};
-    }
+  transition: border-color 0.2s;
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  &:hover {
+    border-color: ${darken(0.1, "#d7d7d7")};
+  }
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
