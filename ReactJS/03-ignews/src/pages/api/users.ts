f38b9e11a31b2d68efa9a337a3eccf2d99fac149
luiz-ortega <luizngust@hotmail.com>
2021-04-17 15:50:12 -0300

    feat: add next backend API with API routes

---
+++ ReactJS/03-ignews/src/pages/api/users.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  const users = [
+    { id: 1, name: "teste" },
+    { id: 2, name: "teste2" },
+    { id: 3, name: "teste3" },
+  ];
+
+  return response.json(users);
+};
