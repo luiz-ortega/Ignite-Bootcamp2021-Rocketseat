dce73bf48a56f43f5ec70f7296f5f4dd165075fc
luiz-ortega <luizngust@hotmail.com>
2021-04-20 23:22:05 -0300

    feat: add subscribe function to subscribeButton component

--- ReactJS/03-ignews/src/components/SubscribeButton/index.tsx
+++ ReactJS/03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 import { signIn, useSession } from "next-auth/client";
+import { api } from "../../services/api";
+import { stripe } from "../../services/stripe";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -8,10 +11,22 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = await response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
     }
   }
   return (
